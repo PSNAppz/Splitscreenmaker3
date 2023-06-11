66422f31b10e31ff8dc71f2b36aab1df9a3d2d38
PSNAppZ <thepsnarayanan@gmail.com>
2023-06-11 17:50:54 +0530

    Patch: Added crop function

--- script.py
+++ script.py
@@ -6,6 +6,29 @@ from fastapi.middleware.cors import CORSMiddleware
 from moviepy.editor import VideoFileClip, clips_array
 from moviepy.editor import *
 
+from moviepy.video.fx.all import crop
+
+def resize_video(clip, width, height):
+    # Determine the aspect ratio of the input clip
+    aspect_ratio = clip.w / clip.h
+    
+    # If the aspect ratio of the input clip is greater than the desired aspect ratio
+    # it means that the clip is wider and we need to crop it horizontally
+    if aspect_ratio > width / height:
+        new_width = int(height * aspect_ratio)
+        new_height = height
+    else:
+        new_width = width
+        new_height = int(width / aspect_ratio)
+
+    # Resize the clip to the new dimensions
+    clip = clip.resize((new_width, new_height))
+
+    # Crop the clip to the desired dimensions
+    clip = crop(clip, width=width, height=height, x_center=clip.w / 2, y_center=clip.h / 2)
+
+    return clip
+
 
 app = FastAPI()
 app.add_middleware(
@@ -14,11 +37,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-
-
-
 
 
 @app.post("/upload")
@@ -40,18 +58,17 @@ async def upload_file(file: UploadFile = File(...), videoNumber: int = Form(...)
 @app.get("/combine")
 async def combine_videos():
     videos = ["video1.mp4", "video2.mp4", "video3.mp4"]
-
-
-
+    
     clip1 = VideoFileClip("video1.mp4")
     clip2 = VideoFileClip("video2.mp4")
     clip3 = VideoFileClip("video3.mp4")
 
-
-
-
     width = 426
     height = 720
+
+    clip1 = resize_video(VideoFileClip("video1.mp4"), width, height)
+    clip2 = resize_video(VideoFileClip("video2.mp4"), width, height)
+    clip3 = resize_video(VideoFileClip("video3.mp4"), width, height)
 
 
     combined = clips_array([[clip1 ,clip2,clip3]])
